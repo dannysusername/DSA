4ccf5a8a0242d0523fd8f43f6270d185c2bdb54e
Daniel Ibarra <dannibar201@gmail.com>
2025-04-29 16:15:23 -0400

    first commit of DSA practice

---
+++ ArrayProblems.java
@@ -0,0 +1,415 @@
+import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class ArrayProblems{
+
+    public static int getSecondLargest(int[] arr){
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+        for(int i = n - 2; i >= 0; i--){
+            if(arr[i] != arr[n - 1]){
+                return arr[i];
+            }
+        }
+
+        return -1;
+    }
+
+    public static int getThirdLargest(int[] arr){
+
+        int n = arr.length;
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        int thirdLargest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] > largest){
+                thirdLargest = secondLargest;
+                secondLargest = largest;
+                largest = arr[i];
+
+            } else if (arr[i] > secondLargest) {
+                thirdLargest = secondLargest;
+                secondLargest = arr[i];
+
+            } else if (arr[i] > thirdLargest) {
+
+                thirdLargest = arr[i];
+
+            } 
+
+        }
+
+        if(thirdLargest == Integer.MIN_VALUE){
+            return -1;
+        } else {
+            return thirdLargest;
+        }
+
+    }
+
+    public static int maximumProductOfATriplet(int[] arr) {
+
+        int largestNumber = Integer.MIN_VALUE;
+        int secondLargestNumber = Integer.MIN_VALUE;
+        int thirdLargestNumber = Integer.MIN_VALUE;
+
+        for(int i = 0; i < arr.length; i++){
+
+            if(arr[i] > largestNumber){
+                thirdLargestNumber = secondLargestNumber;
+                secondLargestNumber = largestNumber;
+                largestNumber = arr[i];
+
+            } else if (arr[i] > secondLargestNumber) {
+                thirdLargestNumber = secondLargestNumber;
+                secondLargestNumber = arr[i];
+
+            } else if (arr[i] > thirdLargestNumber) {
+                thirdLargestNumber = arr[i];
+
+            }
+
+        }
+
+        // { -6, -10, -2, -1, -20 }
+
+        return largestNumber * secondLargestNumber * thirdLargestNumber;
+
+
+    }
+
+    public static ArrayList<Integer> leadersInArray(int[] array){
+
+        // [16, 17, 4, 3, 5, 2]
+        int index = 0;
+        ArrayList<Integer> leaders = new ArrayList<>();
+
+        for(int i = 0; i < array.length; i++){
+
+            if(array[index] < array[i]){
+                index++;
+                i = index;
+            } else if (i == (array.length - 1)){
+                leaders.add(array[index]);
+                i = index;
+                index++;
+            }
+
+        }
+
+        return leaders;
+
+    }
+
+    public static ArrayList<Integer> suffLeadersInArray(int[] array) {
+
+        ArrayList<Integer> suffLeadersInArray = new ArrayList<>();
+
+        int n = array.length;
+        int maxRight = array[n - 1];
+        suffLeadersInArray.add(maxRight);
+
+        for(int i = n - 2; i >= 0; i--){
+            if(array[i] > maxRight){
+                suffLeadersInArray.add(array[i]);
+                maxRight = array[i];
+            }
+        }
+
+        Collections.reverse(suffLeadersInArray);
+
+        return suffLeadersInArray;
+
+    }
+
+    public static boolean isArraySorted(int[] array, int n){
+
+        // [10, 20, 40, 50, 60]
+
+        if(n == 0 || n == 1){
+            return true;
+        }
+
+
+        return array[n - 1] >= array[n - 2] && 
+            isArraySorted(array, n - 1);
+
+    }
+
+    public static int removeDuplicatesFromSortedArray(int[] array){
+
+        // 1, 2, 2, 3, 4, 4, 4, 5, 5
+        int n = array.length;
+
+        if(n <= 1){
+            return n;
+        }
+
+        int idx = 1;
+
+        for(int i = 1; i < n; i++){
+
+            if(array[i] != array[i - 1]){
+                array[idx++] = array[i];
+            }
+
+        }
+
+        return idx;
+
+    }
+
+    public static void generatingAllSubArrays(ArrayList<Integer> array){
+
+        int n = array.size();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                for(int k = i; k <= j; k++){
+                    System.out.println(array.get(k));
+                }
+                System.out.println();
+            }
+        }
+
+    }
+
+    public static void recursiveSubArrays(int[] array, int start, int end){
+
+        if (end == array.length){
+            return;
+
+        } else if(start > end) {
+            recursiveSubArrays(array, 0, end + 1);
+
+        } else {
+            System.out.print("[");
+            for(int i = start; i < end; i++){
+                System.out.print(array[i] + ", ");
+            }
+            System.out.println(array[end] + "]");
+            recursiveSubArrays(array, start + 1, end);
+
+        }
+        return;
+
+    }
+
+    public static int[] reverseArray(int[] array){
+        int[] reversedArray = array;
+        int start = 0;
+        int end = array.length - 1;
+
+        while(start < end){
+            int temp = reversedArray[end];
+            reversedArray[end--] = reversedArray[start];
+            reversedArray[start++] = temp;
+
+        }
+
+        return reversedArray;
+
+    }
+
+    public static void recursiveReverseArray(int[] array, int start, int end){
+        
+        if(start >= end){
+            return;
+        }
+
+        int temp = array[end];
+        array[end] = array[start];
+        array[start] = temp;
+        
+        recursiveReverseArray(array, ++start, --end);
+
+    }
+
+    public static void rotateArrayCW(int[] array, int d){
+
+        int n = array.length;
+        
+
+        for(int i = 0; i < 2; i++){
+            int temp1 = 0;
+            int temp2 = array[0];
+            for(int j = 0; j < n; j++){
+                temp1 = array[(j + 1) % n];            
+                array[(j + 1) % n] = temp2;           
+                temp2 = temp1;            
+            }
+
+        }  
+        //[1, 2, 3, 4, 5, 6]
+         //[1, 1, 3, 4, 5, 6] temp1 = 2; temp2 = 2; i = 0;
+         //[1, 1, 2, 4, 5, 6] temp1 = 3; temp2 = 3; i = 1;
+         //[1, 1, 2, 3, 5, 6] temp1 = 4; temp2 = 4; i = 2;
+         //[1, 1, 2, 3, 4, 6] temp1 = 5; temp2 = 5; i = 3;
+         //[1, 1, 2, 3, 4, 5] temp1 = 6; temp2 = 6; i = 4;
+         //[6, 1, 2, 3, 4, 5] temp1 = 1; temp2 = 1; i = 5;
+
+    }
+
+    public static void rotateArrayCCW(int[] array, int d){
+
+        int n = array.length;
+
+        for(int i = 0; i < d; i++){
+            int temp1 = 0;
+            int temp2 = array[n - 1];
+
+            for(int j = n - 1 ; j > 0; j--){
+                temp1 = array[(j - 1) % n];            
+                array[(j - 1) % n] = temp2;           
+                temp2 = temp1;
+
+            }
+
+        }  
+        //[1, 2, 3, 4, 5, 6]
+         //[1, 1, 3, 4, 5, 6] temp1 = 2; temp2 = 2; i = 0;
+         //[1, 1, 2, 4, 5, 6] temp1 = 3; temp2 = 3; i = 1;
+         //[1, 1, 2, 3, 5, 6] temp1 = 4; temp2 = 4; i = 2;
+         //[1, 1, 2, 3, 4, 6] temp1 = 5; temp2 = 5; i = 3;
+         //[1, 1, 2, 3, 4, 5] temp1 = 6; temp2 = 6; i = 4;
+         //[6, 1, 2, 3, 4, 5] temp1 = 1; temp2 = 1; i = 5;
+
+    }
+
+    public static int[] tempArrayRotate(int[] array, int d){
+
+        int n = array.length;
+        int[] temp = new int[n];
+
+        d %= n;
+ 
+            for(int i = 0; i < d; i++) {
+                temp[i] = array[n - d + i];
+            }
+
+            for(int i = 0; i < n - d; i++) {
+                temp[i + d] = array[i]; 
+            }
+
+            for(int i = 0; i < n; i++){
+                array[i] = temp[i];
+            }
+
+            return array;
+        
+
+    }
+
+    public static void JugglingAlgorithm(int[] array, int d){
+
+       
+        int n = array.length;
+        int nextEle;
+        int currEle = array[0];
+        d %= n;
+
+        for(int i = 0; i < n/2; i++){
+            nextEle = array[((i * d) + d) % n];
+            array[((i * d) + d) % n] = currEle;
+            currEle = nextEle;
+            
+        }
+
+    }
+
+    public static void moveZerosToEnd(int[] array){
+
+        int curIdx = 0;
+
+        for(int i = 0; i < array.length; i++){
+
+            if(array[i] != 0){
+                int temp = array[curIdx];
+                array[curIdx] = array[i];
+                array[i] = temp; // 0
+                curIdx++;
+            } 
+
+        }
+
+    }
+
+    public static int minIncByKOperations(int array[], int k){
+
+        int largestNum = array[0];
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] > largestNum){
+                largestNum = array[i];
+            }
+
+            if(array[i] % k != 0){
+                return -1;
+            }
+        }
+
+        int totalOperations = 0;
+
+        for(int i = 0; i < array.length; i++){
+
+            if(array[i] != largestNum){
+                int total = (largestNum - array[i]);
+                totalOperations += total/k;
+            }
+
+        }
+
+        return totalOperations;
+
+    }
+
+    public static int minSumOfCosts(int[] array){
+        
+        int smallestNumber = array[0];
+        int smnIdx = 0;
+        int n = array.length;
+
+        for(int i = 0; i < n; i++){
+            if(array[i] < smallestNumber){
+                smallestNumber = array[i];
+                smnIdx = i;
+            }
+        }
+
+        int cost = (n - 1) * smallestNumber;
+
+        return cost;
+
+    }
+
+    public static void printAltElements(int[] array, int i){
+        
+        if(i >= array.length){
+            return;
+        }
+
+        System.out.println(array[i]);
+        i += 2;
+        printAltElements(array, i);
+        
+    }
+               
+
+    public static void main(String[] args) {
+
+       int[] array = {4, 2, 7, 8};
+        printAltElements(array, 0);
+       
+        
+    }
+
+}
+
+
+    
+
---
+++ Arrays.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+
+public class Arrays {
+    public static void main(String [] args){
+
+        /*
+
+        1. Arrays based on size
+
+        Fixed sized array
+
+        */
+
+        int array[] = {1,2,3,4,5};
+        
+        int []array2 = new int[3];
+         
+        //Dynamically sized array memory is allocated and deallocated in these arrays
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(5);
+        arr.add(2);
+        arr.add(3);
+        
+        // [5, 2, 3]
+
+        /*
+        
+        2. Arrays based on dimensions 
+
+        One-dimensional Array
+
+        */
+
+        int row[] = {1, 2, 3, 4, 5};
+
+        /*
+        
+        Multi-dimensional Array
+            Used to store complex data in the form of tables. Can have 2-D arrays, 3-D arrays, 4-D arrays and so on.
+
+        -Two Dimensional Array (2DArray or Matrix) considered array of arrays or as a matirx consisting of rows and columns
+        */
+     
+        int[][] twodarray;
+
+        twodarray = new int[2][2]; // 2 Dimensional fixed size array with 2 rows and 2 columns
+
+        int[][] twodarray2 = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+        /* 
+        -Three Dimensional Array (3DArray) A 3-D Multidimensional Array contains three dimensions so it can be considered an array of two-dimensional arrays
+
+        */
+
+        int [][][] threedarray;
+
+        threedarray = new int[3][3][3];
+
+        int [][][] threedarray2 = {
+            {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+            },
+            {
+                {13, 14, 15, 16},
+                {17, 18, 19, 20},
+                {21, 22, 23, 24}
+            }
+        };
+
+        /* 
+
+        3. Operations on Array - when arrays are created it occupies a continguous block of memory and elements are stored in an indexed manner
+
+        =Array Traversal - accessing each element from start to end or in reverse by using a loop
+
+        Types of array traversal:
+
+        -Sequential Linear Traversal
+        -Reverse Traversal
+
+        =Insertion in Array - inserting a element at a specific position while mainting order of the existing elements, often requires shifting existing elements 
+        to make space and updating the size. Depending on the position of the new element depends on the time complexity of the insertion operation.
+
+        =Deleting in Array - deletion in an array involves shifting the elements to cover the element that needs to be deleted and updating the size.
+
+        =Searching in Array - searches through an array to find a element in a list.
+
+        -Linear Search(Sequential Search) - traverses array one element at a time and compares each element with target value, if match found returns index of the element
+
+        -Binary Search(Effecient search for sorted arrays) - repeatedly divdes the search space in half until the target element is found
+
+
+
+
+        */
+
+
+    }
+
+
+}
+
